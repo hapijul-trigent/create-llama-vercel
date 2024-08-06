bf7e9965a645c832b7f1276d3bdc1ccc820b0f79
hapijul-trigent <hapijul_h@trigent.com>
2024-08-06 06:10:03 +0000

    migrations

---
+++ backend/app/migrations/versions/14163f1e4388_llmnoteadded.py
@@ -0,0 +1,26 @@
+"""llmNoteadded
+
+Revision ID: 14163f1e4388
+Revises: 
+Create Date: 2024-08-06 05:58:03.912666
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '14163f1e4388'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
